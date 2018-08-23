a231cfd176fac1e69f4495694fe465471d852c0c
sonice33 <gglsy0128@naver.com>
2018-08-24 01:40:37 +0900

    faq js추가 푸터추가

--- js/script.js
+++ js/script.js
@@ -37,4 +37,24 @@ $(document).ready(function(){
 	},3000);
 	
 	
+	//faq 버튼 누르면 해당 질문 목록 보이기
+	$(".faq_btn li").click(function(event){
+		event.preventDefault();
+		
+		var list= $(this).index();
+		
+		$(".faq_btn li").removeClass("on");
+		$(this).addClass("on");
+		
+		$(".faq_list ul").hide();
+		$(".faq_list ul").eq(list).show();
+	});
+	
+	//faq 질문 누르면 답변 토글
+	$(".faq_list .question").click(function(){
+		$(this).siblings(".answer").slideToggle();
+		$(this).children(".q_title").toggleClass("on");
+	});
+	
+	
 });
